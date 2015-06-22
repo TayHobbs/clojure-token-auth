82e6a2c676ac7049452da3f433b58f106db699e6
Taylor Hobbs <taylor@feemon.com>
2015-06-22 17:56:52 -0500

    Dependency: Use environ for environment variables

---
+++ profiles.clj
@@ -0,0 +1,2 @@
+{:dev-env-vars  {:env {:database-url  "postgres://auth_user:asdf@127.0.0.1:5432/token_auth"}}
+ :test-env-vars {:env {:database-url  "postgres://auth_user:asdf@127.0.0.1:5432/token_auth_test"}}}
--- project.clj
+++ project.clj
@@ -1,12 +1,24 @@
 (defproject token-auth "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Token Authentication"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [compojure           "1.3.1"]
+                 [ring/ring-defaults  "0.1.2"]
+                 [environ             "1.0.0"]]
+
+  :plugins [[lein-environ "1.0.0"]
+            [lein-ring    "0.8.13"]]
+
   :ring {:handler token-auth.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+
+  :profiles {:test-local   {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                           [ring-mock                 "0.1.5"]]}
+
+             ;; Set these in ./profiles.clj
+             :test-env-vars {}
+             :dev-env-vars  {}
+
+             :test       [:test-local :test-env-vars]
+             :dev        [:dev-env-vars :test-local]})
