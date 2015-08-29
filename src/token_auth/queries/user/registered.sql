11a6d8221cf74bca559bd5a2c94d12c13d6a060d
Taylor Hobbs <taylor@feemon.com>
2015-08-29 18:50:14 -0500

    Feature: Add queries to get user by email or username

--- src/token_auth/queries/user/registered.sql
+++ src/token_auth/queries/user/registered.sql
@@ -6,6 +6,24 @@ SELECT id
        , refresh_token
 FROM registered_user;
 
+-- name: get-user-by-email
+-- Selects user by email
+SELECT id
+       , email
+       , username
+       , refresh_token
+FROM   registered_user
+WHERE  email = :email;
+
+-- name: get-user-by-username
+-- Selects user by username
+SELECT id
+       , email
+       , username
+       , refresh_token
+FROM   registered_user
+WHERE  username = :username;
+
 -- name: insert-user<!
 -- inserts a user
 INSERT INTO registered_user (
